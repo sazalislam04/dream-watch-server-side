82739c6212a07a000774d6e7345a5079ef22ed45
sazalislam04 <mdsazalislam04@gmail.com>
2022-11-24 03:43:23 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,6 +11,17 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Dream Wathces server running");
 });
+
+// mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bts619l.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+console.log(uri);
 
 app.listen(port, () => {
   console.log(`Dream Watches server running on port ${port}`);
